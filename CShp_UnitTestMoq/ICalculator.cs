b877947c8877ed1fc3bdb8d91e3e007f8250ade6
A. Marques Da Silva <a.marques.da.silva@avanade.com>
2020-03-26 15:56:13 -0300

    Add project files.

---
+++ CShp_UnitTestMoq/ICalculator.cs
@@ -0,0 +1,7 @@
+﻿namespace CShp_UnitTestMoq
+{
+    public interface ICalculator
+    {
+        (string op, double result) Calculate(string op, double a, double b);
+    }
+}
---
+++ CShp_UnitTestMoq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CShp_UnitTestMoq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
